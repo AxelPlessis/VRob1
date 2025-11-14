8804b124dfbe62e6f19f0b8b16224a735d2ae221
Laïcan <167437065+damien-vailland@users.noreply.github.com>
2025-11-14 09:28:17 +0100

    main

---
+++ VRob1/main.cpp
@@ -0,0 +1,45 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio/registry.hpp>
+#include <fstream>
+
+int main()
+
+{
+
+    std::string path = "ressources/video1.mp4";
+    std::ifstream test(path);
+    if (!test.good()) {
+        std::cerr << "File not found or inaccessible: " << path << std::endl;
+        return -1;
+    }
+
+    std::cout << "Chemin passé à OpenCV : [" << path << "]" << std::endl;
+
+    cv::VideoCapture cap(path);
+
+
+
+    if (!cap.isOpened()) {
+        std::cerr << "Error: Could not open video file." << std::endl;
+        return -1;
+    }
+
+    cv::Mat frame;
+    while (true) {
+        cap >> frame;
+        if (frame.empty()) {
+            break; // End of video
+        }
+        cv::imshow("Video Frame", frame);
+        if (cv::waitKey(30) >= 0) {
+            break; // Exit on any key press
+        }
+    }
+    cap.release();
+    cv::destroyAllWindows();
+    return 0;
+}
