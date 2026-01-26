42a4b825292558969ca18dbb4bffe3dd53be292e
Laïcan <167437065+damien-vailland@users.noreply.github.com>
2026-01-26 10:36:12 +0100

    tt

--- VRob1/main.cpp
+++ VRob1/main.cpp
@@ -45,7 +45,7 @@ int main()
 		return -1;
 	}
 
-	cout << "Chemin passe a OpenCV : [" << path << "]" << endl;
+	cout << endl << "Chemin passe a OpenCV : [" << path << "]" << endl << endl;
 
 	VideoCapture cap(path, CAP_FFMPEG);
 
@@ -71,27 +71,34 @@ int main()
 	fs["cameraMatrix"] >> K;
 	fs["dist_coeffs"] >> dist;
 	fs.release();
-	std::cout << "Matrice camera K:\n" << K << "\n\n";
-	std::cout << "dist:\n" << dist << "\n";
+	std::cout << "Matrice camera K:\n" << K << endl << endl;
+	std::cout << "dist:\n" << dist << endl << endl;
 
 
 	// POINTS EN PIXELS ET EN CENTIMETRE
 
-	vector<Point2f> corners = {
+	vector<Point2f> zero_x_i_ref = {
 		Point2f(376, 96),
 		Point2f(497, 72),
 		Point2f(558, 266),
 		Point2f(433, 299)
 	};
 	
-	vector<Point3f> objectPoints = {
+	vector<Point3f> w_X_i_ref = {
+		{0,0, 0},
+		{2.3,0, 0},
+		{2.3,2., 0},
+		{0,2., 0}
+	};
+
+	/*vector<Point3f> objectPoints = {
 		{0,0, 0},
 		{2.9,0, 0},
 		{2.0,2.3, 0},
 		{0,2.8, 0}
-	};
+	};*/
 
-	vector<Point2f> refPts = transform(corners, K);
+	vector<Point2f> refPts = transform(zero_x_i_ref, K);
 
 	cout << "refpts:" << refPts << endl;
 
@@ -99,22 +106,22 @@ int main()
 	//  CONSTRUCTION DE LA MATRICE HOMOGENE cam_T_w
 	
 	Mat rvec, tvec;
-	solvePnP(objectPoints, corners, K, dist, rvec, tvec);
+	solvePnP(w_X_i_ref, zero_x_i_ref, K, dist, rvec, tvec);
 	//cout << "rvec: " << rvec.t() << endl;
 	//cout << "tvec: " << tvec.t() << endl;
 
 	Mat R;
 	Rodrigues(rvec, R);
-	//cout << "R: " << R << endl;
+	cout << "R: " << R << endl << endl;
 
 	// Repere plan
-	cv::Mat cam_T_w = (cv::Mat_<double>(4, 4) <<
+	cv::Mat zero_T_w = (cv::Mat_<double>(4, 4) <<
 		R.at<double>(0, 0), R.at<double>(0, 1), R.at<double>(0, 2), tvec.at<double>(0),
 		R.at<double>(1, 0), R.at<double>(1, 1), R.at<double>(1, 2), tvec.at<double>(1),
 		R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2), tvec.at<double>(2),
 		0, 0, 0, 1
 	);
-	cout << "Matrice homogene de la camera: \n" << cam_T_w << endl;
+	cout << "Matrice homogene de la camera:" << endl << zero_T_w << endl << endl;
 	
 
 	//  DEBUT DU SIFT
@@ -123,18 +130,20 @@ int main()
 
 	std::vector<KeyPoint> keypoints;
 	Mat descriptors;
-
+	
 	sift->detectAndCompute(frame, noArray(), keypoints, descriptors);
 
 	Mat output;
 	drawKeypoints(frame, keypoints, output, Scalar::all(255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-	imshow("SIFT keypoints", output);
+
+	// ==== IMAGE AVEC POINTS SIFT ====
+	//imshow("SIFT keypoints", output);
 
 
 
 	//   RECUPERATION SEULEMENT POINTS SIFTS DANS LA ZONE 
 
-	// Coins mais agrandis pour accueillir les kp importants
+	// Coins + padding (pour accueillir les kp importants)
 	vector<Point> zone = {
 		Point(366, 86),
 		Point(507, 62),
@@ -142,8 +151,8 @@ int main()
 		Point(423, 309)
 	};
 
-	vector<KeyPoint> filteredKP;
-	Mat filteredDesc;
+	vector<KeyPoint> filtered_kp;
+	Mat desc_zero;
 
 	for (int i = 0; i < keypoints.size(); i++) {
 		Point2f p = keypoints[i].pt;
@@ -151,8 +160,8 @@ int main()
 		double inside = pointPolygonTest(zone, p, false);
 
 		if (inside >= 0) {  
-			filteredKP.push_back(keypoints[i]);
-			filteredDesc.push_back(descriptors.row(i));
+			filtered_kp.push_back(keypoints[i]);
+			desc_zero.push_back(descriptors.row(i));
 		}
 	}
 
@@ -161,29 +170,31 @@ int main()
 	vector<vector<Point>> contours = {zone};
 	polylines(imgColor, contours, true, Scalar(0, 255, 0), 2);
 
-	drawKeypoints(imgColor, filteredKP, imgColor, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-	imshow("KP in zone", imgColor);
+	drawKeypoints(imgColor, filtered_kp, imgColor, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	
+	// ==== IMAGE AVEC KEYPOINTS DANS LA ZONE ====
+	//imshow("KP in zone", imgColor);
 
 
 	//  CALCUL DES POSITIONS DES POINTS EN 3D DANS REPERE Rw
 
-	vector<Point3d> kp3d;
+	vector<Point3d> w_X_i;
 
 	cv::Mat R_inv = R.t();
 	cv::Mat t_inv = -R_inv * tvec;
 
-	cv::Mat w_T_cam = cv::Mat::eye(4, 4, CV_64F);
+	cv::Mat w_T_zero = cv::Mat::eye(4, 4, CV_64F);
 
-	R_inv.copyTo(w_T_cam(cv::Rect(0, 0, 3, 3)));
-	t_inv.copyTo(w_T_cam(cv::Rect(3, 0, 1, 3)));
+	R_inv.copyTo(w_T_zero(cv::Rect(0, 0, 3, 3)));
+	t_inv.copyTo(w_T_zero(cv::Rect(3, 0, 1, 3)));
 
-	cv::Point3d O_cam(w_T_cam.at<double>(0, 3),
-		w_T_cam.at<double>(1, 3),
-		w_T_cam.at<double>(2, 3));
+	cv::Point3d O_cam(w_T_zero.at<double>(0, 3),
+		w_T_zero.at<double>(1, 3),
+		w_T_zero.at<double>(2, 3));
 
-	for (int i = 0; i < filteredKP.size(); i++) {
-		double x_norm = (filteredKP[i].pt.x - K.at<double>(0, 2)) / K.at<double>(0, 0);
-		double y_norm = (filteredKP[i].pt.y - K.at<double>(1, 2)) / K.at<double>(1, 1);
+	for (int i = 0; i < filtered_kp.size(); i++) {
+		double x_norm = (filtered_kp[i].pt.x - K.at<double>(0, 2)) / K.at<double>(0, 0);
+		double y_norm = (filtered_kp[i].pt.y - K.at<double>(1, 2)) / K.at<double>(1, 1);
 
 		cv::Mat ray_cam = (cv::Mat_<double>(3, 1) << x_norm, y_norm, 1.0);
 
@@ -195,67 +206,92 @@ int main()
 		if (std::abs(d_world.z) > 1e-6) {
 			double lambda = -O_cam.z / d_world.z;
 			cv::Point3d X_world = O_cam + lambda * d_world;
-			kp3d.push_back(X_world);
+			w_X_i.push_back(X_world);
 		}
 	}
 
 	// --- VÉRIFICATION PAR REPROJECTION ---
 
-	vector<Point3f> kp3d_float;
-	for (const auto& p : kp3d) {
-		kp3d_float.push_back(Point3f((float)p.x, (float)p.y, (float)p.z));
+	vector<Point3f> w_X_i_float;
+	for (const auto& p : w_X_i) {
+		w_X_i_float.push_back(Point3f((float)p.x, (float)p.y, (float)p.z));
 	}
-
-	// 2. Préparez le vecteur de sortie explicitement
 	vector<Point2f> projectedPoints;
 
 	try {
-		if (!kp3d_float.empty()) {
+		if (!w_X_i_float.empty()) {
 			// K, dist, rvec, tvec peuvent rester en double (CV_64F)
-			cv::projectPoints(kp3d_float, rvec, tvec, K, dist, projectedPoints);
+			cv::projectPoints(w_X_i_float, rvec, tvec, K, dist, projectedPoints);
 		}
 	}
 	catch (const cv::Exception& e) {
 		cerr << "Erreur : " << e.what() << endl;
 	}
 
-	// Création d'une image pour afficher la différence
 	Mat imgCheck = frame.clone();
 	for (size_t i = 0; i < projectedPoints.size(); i++) {
-		circle(imgCheck, filteredKP[i].pt, 4, Scalar(0, 0, 255), -1);
-
+		circle(imgCheck, filtered_kp[i].pt, 4, Scalar(0, 0, 255), -1);
+		
 		circle(imgCheck, projectedPoints[i], 2, Scalar(0, 255, 0), -1);
-
 	}
 
+	// ==== IMAGE DE REPROJECTION ====
 	imshow("Verification Reprojection", imgCheck);
 
-	if (!kp3d_float.empty()) {
-		cout << "Exemple de point 3D calculé (Rw) : " << kp3d_float[0] << endl;
-
+	if (!w_X_i_float.empty()) {
+		cout << "Exemple de point 3D calcule (Rw) : " << w_X_i_float[0] << endl;
 	}
 
+	Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+	if (desc_zero.type() != CV_32F) desc_zero.convertTo(desc_zero, CV_32F);
+
 	//  LECTURE DE LA VIDEO
+	
 
 	while (true) {
 
 		// Commenter = une seule frame
 		cap >> frame; 
 
-		vector<KeyPoint> kp_k;
-		Mat desc_k;
-			 
-		sift->detectAndCompute(frame, noArray(), kp_k, desc_k);
-
+		// Repeter en boucle la video
 		if (frame.empty()) {
 			break;
 		}
+
+		// SIFT
+		vector<KeyPoint> keypoint_k;
+		Mat desc_k;
+		sift->detectAndCompute(frame, noArray(), keypoint_k, desc_k);
+
+		if (!desc_k.empty()) {
+
+			vector<vector<DMatch>> knn_matches;
+			matcher->knnMatch(desc_zero, desc_k, knn_matches, 1);
+
+			vector<Point2f> k_x_i;
+			vector<Point3f> k_X_i;
+
+			for (size_t i = 0; i < knn_matches.size(); i++) {
+				k_x_i.push_back(keypoint_k[i].pt);
+				k_X_i.push_back(w_X_i_float[i]);
+			}
+
+			if (k_x_i.size() >= 4) {
+				Mat rvec_k, tvec_k;
+
+				solvePnP(k_X_i, k_x_i, K, dist, rvec_k, tvec_k);
+				drawFrameAxes(frame, K, dist, rvec_k, tvec_k, 1.0);
+			}
+		}
+
+		
 		imshow("Video Frame", frame);
+
 		if (waitKey(30) >= 0) {
 			break;
 		}
 	}
 	cap.release();
-	destroyAllWindows();
+	//destroyAllWindows();
 	return 0;
 }
